4cf3ab37f27cd7d7e308ad3bd28ca70bd62554eb
Phruela <pedro.hlr17@gmail.com>
2025-09-26 15:42:58 -0300

    Primeiro commit. Adicionando a função display_menu

---
+++ interface_usuario.py
@@ -0,0 +1,16 @@
+def display_menu():
+    '''Exibe o menu principal para o usuário.
+    
+    Imprime todas as opções disponíveis que o usuário
+    pode escolher para gerenciar os eventos'''
+
+    print('''
+    === Planejador de Eventos do Campus ===
+    1. Ver todos os eventos
+    2. Adicionar Evento
+    3. Filtrar Categoria
+    4. Marcar Evento como Participado
+    5. Gerar Relatório
+    6. Excluir Evento
+    7. Sair
+    ''')
