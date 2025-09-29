5b62a7c2711616a80c80b3243f2f81db0c2e73d4
Júlia Peghini <juliapeghini@gmail.com>
2025-09-29 20:14:58 -0300

    chore: Integração e o arquivo main

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
+# main.py - Versão final e integrada
 from interface_usuario import *
 # from gerenciador_eventos import *
+import gerenciador_eventos as ge
+import interface_usuario as iu
 import os
 import time
 
 def main():
-    while(True):
+    eventos = ge.carregar_dados()
+
+    while True:
         os.system("cls" if os.name == "nt" else "clear")
-        escolha = get_escolha()
+        iu.display_menu()
+        escolha = iu.get_escolha()
         match escolha:
-        #     # case 1:
-        #     #     listar_eventos()
-        #     # case 2:
-        #     #     adicionar_evento()
+            case 1:
+                ge.listar_eventos(eventos)
+            case 2:
+                ge.adicionar_evento(eventos)
             case 3:
                 categoria = input('Informe a categoria a ser filtrada: ')
-                filtrar_por_categoria(lista_eventos, categoria)
-                print()
-                print('Pressione Enter para continuar.')
-                input()
+                iu.filtrar_por_categoria(eventos, categoria)
             case 4:
-                id_evento = int(input("Informe o ID do evento para marcar a participação: "))
-                marcar_evento_atendido(lista_eventos, id_evento)
-                print()
-                print('Pressione Enter para continuar')
-                input()
+                try:
+                    id_evento = int(input("Informe o ID do evento para marcar a participação: "))
+                    iu.marcar_evento_atendido(eventos, id_evento)
+                    ge.salvar_dados(eventos)
+                except ValueError:
+                    print("ID inválido. Por favor, digite um número.")
             case 5:
-                gerar_relatorio(lista_eventos)
-                print()
-                print('Pressione Enter para continuar')
-                input()
-        #     # case 6:
-        #     #     deletar_evento()
+            
+                iu.gerar_relatorio(eventos)
+            case 6:
+                print("\n--- Excluir Evento ---")
+                ge.listar_eventos(eventos)
+                
+                if eventos:
+                    try:
+                        id_para_remover = int(input("Digite o ID do evento que você deseja excluir: "))
+                        sucesso = ge.remover_evento(eventos, id_para_remover)
+                        if sucesso:
+                            ge.salvar_dados(eventos)
+                    except ValueError:
+                        print("Erro: ID inválido. Por favor, digite um número.")
             case 7:
+                print("Saindo do programa...")
                 break
             case _:
                 print('Opção Inválida')
                 time.sleep(2)
-
+        
+        if escolha != 7:
+            print('\nPressione Enter para continuar.')
+            input()
 
 if __name__ == "__main__":
-    main()
+        main()
